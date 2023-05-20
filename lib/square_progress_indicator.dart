0f9f4699bb9c854734c02f57c043688fbe3f0570
AmirAbbas <a.abbasj@yahoo.com>
2023-05-20 15:52:50 +0330

    feat: first beta code

--- lib/square_progress_indicator.dart
+++ lib/square_progress_indicator.dart
@@ -6,140 +6,197 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class SquareProgressIndicator extends StatelessWidget {
+  ///The width of rectangle that the progress line is drawn around it.
   final double width;
+
+  ///The height of rectangle that the progress line is drawn around it.
   final double height;
-  final double value;
 
-  ///square border radius
+  ///The value of the progress, it should be between 0 and 1.
+  final double? value;
+
+  ///The border radius of the rectangle, it is applied to all four corners.
   final double borderRadius;
-  final Color progressColor;
-  final Color shadowColor;
 
-  ///thickness of the progress
+  ///The color of the progress line.
+  final Color? color;
+
+  ///The color of the line behind the progress line which show for reminding progress.
+  final Color? emptyStrokeColor;
+
+  ///The width of the progress line.
   final double strokeWidth;
-  final double shadowWidth;
+
+  ///The width of the line behind the progress line which show for reminding progress.
+  final double emptyStrokeWidth;
+
+  ///The child widget, it can be a text or everything you need.
   final Widget? child;
 
-  ///if true the progress is moving clockwise
-  final bool reverse;
+  ///The direction of turn of progress line, if you pass false, the progress line will be reversed, default value is true.
+  final bool clockwise;
 
-  final double startFrom;
+  ///Start position of progress line relative to the topCenter, you can pass a value from [StartPosition] class or custom double value you need.
+  final double startPosition;
+
+  ///The stroke align of the progress line, pass a value from [SquareStrokeAlign] and read it's documents. see: https://api.flutter.dev/flutter/painting/BorderSide/strokeAlign.html
+  final SquareStrokeAlign strokeAlign;
+
+  ///The stroke cap of the progress line and empty line, see: https://api.flutter.dev/flutter/dart-ui/StrokeCap.html
+  final StrokeCap? strokeCap;
 
   const SquareProgressIndicator({
     super.key,
-    this.value = 0.0,
-    this.reverse = false,
+    this.value,
+    this.clockwise = true,
     this.borderRadius = 8,
-    this.progressColor = Colors.blue,
-    this.shadowColor = Colors.grey,
-    this.strokeWidth = 5,
-    this.shadowWidth = 5,
+    this.color,
+    this.emptyStrokeColor = Colors.grey,
+    this.strokeWidth = 4,
+    this.emptyStrokeWidth = 0,
     this.child,
-    this.startFrom = 0,
-    this.width = 150,
-    this.height = 150,
-  });
+    this.startPosition = 0,
+    this.width = 38,
+    this.height = 38,
+    this.strokeAlign = SquareStrokeAlign.inside,
+    this.strokeCap,
+  }) : assert(startPosition >= 0 && startPosition <= 1, "'startFrom' must be between 0 and 1");
 
   @override
   Widget build(BuildContext context) {
+    if (value == null) {
+      return _IndeterminateSquareProgressIndicator(
+        clockwise: clockwise,
+        borderRadius: borderRadius,
+        color: color ?? Theme.of(context).progressIndicatorTheme.color ?? Colors.blue,
+        emptyStrokeColor:
+            emptyStrokeColor ?? Theme.of(context).progressIndicatorTheme.circularTrackColor ?? Colors.transparent,
+        strokeWidth: strokeWidth,
+        emptyStrokeWidth: emptyStrokeWidth,
+        startPosition: startPosition,
+        width: width,
+        height: height,
+        strokeAlign: strokeAlign,
+        strokeCap: strokeCap,
+      );
+    }
     return CustomPaint(
       size: Size(width, height),
-      painter: RadialPainter(
-        startFrom: startFrom,
-        progress: value,
-        color: progressColor,
-        shadowColor: shadowColor,
-        reverse: reverse,
+      painter: _SquareProgressIndicatorStrokePainter(
+        startPosition: startPosition,
+        value: value!,
+        color: color ?? Theme.of(context).progressIndicatorTheme.color ?? Colors.blue,
+        emptyStrokeColor:
+            emptyStrokeColor ?? Theme.of(context).progressIndicatorTheme.circularTrackColor ?? Colors.transparent,
+        clockwise: clockwise,
         strokeWidth: strokeWidth,
-        shadowWidth: shadowWidth,
+        emptyStrokeWidth: emptyStrokeWidth,
         borderRadius: borderRadius,
+        strokeAlign: strokeAlign,
+        strokeCap: strokeCap,
       ),
       child: child,
     );
   }
 }
 
-class RadialPainter extends CustomPainter {
-  final double progress;
+class _SquareProgressIndicatorStrokePainter extends CustomPainter {
+  final double value;
   final Color color;
-  final Color shadowColor;
+  final Color emptyStrokeColor;
   final double strokeWidth;
-  final double shadowWidth;
+  final double emptyStrokeWidth;
   final double borderRadius;
-  final bool reverse;
-  final double startFrom;
+  final bool clockwise;
+  final double startPosition;
+  final SquareStrokeAlign strokeAlign;
+  final StrokeCap? strokeCap;
 
-  RadialPainter({
-    required this.progress,
+  _SquareProgressIndicatorStrokePainter({
+    required this.value,
     this.color = Colors.blue,
-    this.shadowColor = Colors.grey,
+    this.emptyStrokeColor = Colors.grey,
     this.strokeWidth = 4,
-    this.shadowWidth = 1,
-    this.reverse = false,
-    this.startFrom = 0,
+    this.emptyStrokeWidth = 1,
+    this.clockwise = false,
+    this.startPosition = 0,
     this.borderRadius = 10,
+    this.strokeAlign = SquareStrokeAlign.inside,
+    this.strokeCap,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
+    Paint strokePaint = Paint()
       ..color = color
       ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke
-      ..strokeCap = borderRadius > 0 ? StrokeCap.round : StrokeCap.square
+      ..strokeCap = strokeCap ?? (borderRadius > 0 ? StrokeCap.round : StrokeCap.square)
       ..strokeJoin = StrokeJoin.miter;
 
-    Paint shadowPaint = Paint()
-      ..color = shadowColor
-      ..strokeWidth = shadowWidth
+    Paint emptyStrokePaint = Paint()
+      ..color = emptyStrokeWidth <= 0 ? Colors.white.withOpacity(0) : emptyStrokeColor
+      ..strokeWidth = emptyStrokeWidth
       ..style = PaintingStyle.stroke
-      ..strokeCap = borderRadius > 0 ? StrokeCap.round : StrokeCap.square
+      ..strokeCap = strokeCap ?? (borderRadius > 0 ? StrokeCap.round : StrokeCap.square)
       ..strokeJoin = StrokeJoin.miter;
 
-    var path = Path();
-    Path dashPath = Path();
+    var emptyStrokePath = Path();
+    var strokePath = Path();
 
-    var topLeft = Offset(borderRadius + strokeWidth / 2, borderRadius + strokeWidth / 2);
-    var topRight = Offset(size.width - borderRadius - strokeWidth / 2, borderRadius + strokeWidth / 2);
-    var bottomRight =
-        Offset(size.width - borderRadius - strokeWidth / 2, size.height - borderRadius - strokeWidth / 2);
-    var bottomLeft = Offset(borderRadius + strokeWidth / 2, size.height - borderRadius - strokeWidth / 2);
+    var strokeOffset = strokeAlign == SquareStrokeAlign.inside
+        ? strokeWidth / 2
+        : strokeAlign == SquareStrokeAlign.outside
+            ? -strokeWidth / 2
+            : 0.0;
 
-    path.arcTo(Rect.fromCircle(center: topLeft, radius: borderRadius), -3 * pi / 4, pi / 4, false);
-    path.lineTo(size.width - borderRadius - strokeWidth / 2, strokeWidth / 2);
-    path.arcTo(Rect.fromCircle(center: topRight, radius: borderRadius), -pi / 2, pi / 2, false);
-    path.lineTo(size.width - strokeWidth / 2, size.height - borderRadius - strokeWidth / 2);
-    path.arcTo(Rect.fromCircle(center: bottomRight, radius: borderRadius), 0, pi / 2, false);
-    path.lineTo(0 + borderRadius + strokeWidth / 2, size.height - strokeWidth / 2);
-    path.arcTo(Rect.fromCircle(center: bottomLeft, radius: borderRadius), pi / 2, pi / 2, false);
-    path.lineTo(0 + strokeWidth / 2, borderRadius + strokeWidth / 2);
-    path.arcTo(Rect.fromCircle(center: topLeft, radius: borderRadius), pi, pi / 4, false);
+    var topLeft = Offset(borderRadius + strokeOffset, borderRadius + strokeOffset);
+    var topRight = Offset(size.width - borderRadius - strokeOffset, borderRadius + strokeOffset);
+    var bottomRight = Offset(size.width - borderRadius - strokeOffset, size.height - borderRadius - strokeOffset);
+    var bottomLeft = Offset(borderRadius + strokeOffset, size.height - borderRadius - strokeOffset);
 
-    for (PathMetric pathMetric in path.computeMetrics()) {
-      dashPath.addPath(
+    if (clockwise) {
+      emptyStrokePath.moveTo(size.width / 2 + strokeWidth / 2, strokeOffset);
+      emptyStrokePath.lineTo(size.width - borderRadius - strokeOffset, strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: topRight, radius: borderRadius), -pi / 2, pi / 2, false);
+      emptyStrokePath.lineTo(size.width - strokeOffset, size.height - borderRadius - strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: bottomRight, radius: borderRadius), 0, pi / 2, false);
+      emptyStrokePath.lineTo(0 + borderRadius + strokeOffset, size.height - strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: bottomLeft, radius: borderRadius), pi / 2, pi / 2, false);
+      emptyStrokePath.lineTo(0 + strokeOffset, borderRadius + strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: topLeft, radius: borderRadius), pi, pi / 2, false);
+      emptyStrokePath.lineTo(size.width / 2 + strokeWidth / 2, strokeOffset);
+    } else {
+      emptyStrokePath.moveTo(size.width / 2 - strokeWidth / 2, strokeOffset);
+      emptyStrokePath.lineTo(0 + strokeOffset + borderRadius, strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: topLeft, radius: borderRadius), -pi / 2, -pi / 2, false);
+      emptyStrokePath.lineTo(0 + strokeOffset, size.height - borderRadius - strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: bottomLeft, radius: borderRadius), -pi, -pi / 2, false);
+      emptyStrokePath.lineTo(size.width - borderRadius - strokeOffset, size.height - strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: bottomRight, radius: borderRadius), pi / 2, -pi / 2, false);
+      emptyStrokePath.lineTo(size.width - strokeOffset, borderRadius + strokeOffset);
+      emptyStrokePath.arcTo(Rect.fromCircle(center: topRight, radius: borderRadius), 0, -pi / 2, false);
+      emptyStrokePath.lineTo(size.width / 2 - strokeWidth / 2, strokeOffset);
+    }
+
+    for (PathMetric pathMetric in emptyStrokePath.computeMetrics()) {
+      var startPos = clockwise ? startPosition : (1 - startPosition);
+      strokePath.addPath(
           pathMetric.extractPath(
-            pathMetric.length * startFrom,
-            pathMetric.length * progress + pathMetric.length * startFrom,
+            pathMetric.length * startPos,
+            pathMetric.length * value + pathMetric.length * startPos,
           ),
           Offset.zero);
-      dashPath.addPath(
+      strokePath.addPath(
           pathMetric.extractPath(
             0,
-            pathMetric.length * (progress - (1 - startFrom)),
+            pathMetric.length * (value - (1 - startPos)),
           ),
           Offset.zero);
     }
 
-    if (reverse) {
-      // dashPath = dashPath
-      //     .transform(Matrix4Transform().rotate(pi / 2).m.storage)
-      //     .transform(Matrix4Transform().flipHorizontally().m.storage);
-    }
-
-    // dashPath = dashPath.transform(Matrix4Transform().rotateByCenter(startAngle.value, size).m.storage);
-
-    canvas.drawPath(path, shadowPaint);
-    if (progress > 0) canvas.drawPath(dashPath, paint);
+    canvas.drawPath(emptyStrokePath, emptyStrokePaint);
+    if (value > 0) canvas.drawPath(strokePath, strokePaint);
   }
 
   @override
@@ -148,12 +205,118 @@ class RadialPainter extends CustomPainter {
   }
 }
 
-class StartFrom {
-  static const topLeft = 0;
-  static const topCenter = .125;
-  static const topRight = .125 * 2;
-  static const rightCenter = .125 * 3;
-  static const bottomRight = .125 * 4;
+class StartPosition {
+  static const topCenter = 0.0;
+
+  ///This works only when width=height
+  static const topRight = .125 * 1;
+  static const rightCenter = .125 * 2;
+
+  ///This works only when width=height
+  static const bottomRight = .125 * 3;
+  static const bottomCenter = .125 * 4;
+
+  ///This works only when width=height
   static const bottomLeft = .125 * 5;
-  static const leftCenter = .125 * 7;
+  static const leftCenter = .125 * 6;
+
+  ///This works only when width=height
+  static const topLeft = .125 * 7;
+}
+
+enum SquareStrokeAlign {
+  ///When you use [center], half of stroke width is drawn inside the rectangle bounds and other half is drawn outside the bounds.
+  center,
+
+  ///When you use [outside], stroke line is drawn outside the rectangle bounds, maybe draw on other widgets.
+  outside,
+
+  ///When you use [inside], stroke line is drawn inside the rectangle bounds, this is default value.
+  inside
+}
+
+class _IndeterminateSquareProgressIndicator extends StatefulWidget {
+  final double width;
+  final double height;
+  final double borderRadius;
+  final Color color;
+  final Color emptyStrokeColor;
+  final double strokeWidth;
+  final double emptyStrokeWidth;
+  final Widget? child;
+  final bool clockwise;
+  final double startPosition;
+  final SquareStrokeAlign strokeAlign;
+  final StrokeCap? strokeCap;
+
+  const _IndeterminateSquareProgressIndicator({
+    Key? key,
+    required this.clockwise,
+    required this.borderRadius,
+    required this.color,
+    required this.emptyStrokeColor,
+    required this.strokeWidth,
+    required this.emptyStrokeWidth,
+    required this.startPosition,
+    required this.width,
+    required this.height,
+    required this.strokeAlign,
+    this.strokeCap,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  State<_IndeterminateSquareProgressIndicator> createState() => _IndeterminateSquareProgressIndicatorState();
+}
+
+class _IndeterminateSquareProgressIndicatorState extends State<_IndeterminateSquareProgressIndicator>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late AnimationController _controller2;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 350 * 8));
+    _controller2 = AnimationController(vsync: this, duration: const Duration(milliseconds: 300 * 2));
+    _controller.repeat();
+    _controller2.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller2.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: (context, w) {
+        return AnimatedBuilder(
+            animation: _controller2,
+            builder: (context, w) {
+              var anim2val = _controller2.drive(CurveTween(curve: Curves.easeInOutQuart)).value * .7;
+              return SquareProgressIndicator(
+                value: anim2val,
+                clockwise: widget.clockwise,
+                borderRadius: widget.borderRadius,
+                color: widget.color,
+                emptyStrokeColor: widget.emptyStrokeColor,
+                strokeWidth: widget.strokeWidth,
+                emptyStrokeWidth: widget.emptyStrokeWidth,
+                startPosition:
+                    (widget.startPosition + (widget.clockwise ? _controller.value : (1 - _controller.value))) % 1,
+                width: widget.width,
+                height: widget.height,
+                strokeAlign: widget.strokeAlign,
+                strokeCap: widget.strokeCap,
+                child: widget.child,
+              );
+            });
+      },
+      animation: _controller,
+    );
+  }
 }
